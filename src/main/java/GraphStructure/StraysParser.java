7cf70f51899e7bd60b9c9b51fb8090792d395cd9
Cagri Tasci <cagri.c.tasci@outlook.de>
2019-03-21 19:15:08 +0100

    updated logging and jar

--- src/main/java/GraphStructure/StraysParser.java
+++ src/main/java/GraphStructure/StraysParser.java
@@ -7,7 +7,6 @@ import de.topobyte.osm4j.core.model.iface.OsmWay;
 import de.topobyte.osm4j.core.model.util.OsmModelUtil;
 import de.topobyte.osm4j.pbf.seq.PbfIterator;
 import gnu.trove.list.TLongList;
-import org.apache.commons.lang3.StringUtils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -29,7 +28,9 @@ public class StraysParser {
 
         Set<String> legalStreetsPEDSTRIAN = new HashSet<>(pedWaysList);
 
+        System.out.println("start counting desirable edges");
         // Count number of ways; needed for array creation.
+
         int numWays = countWays(legalStreetsPEDSTRIAN);
         //int numWays = 124565264;
         System.out.println(String.format("Number of way counted: %s", numWays));
@@ -42,14 +43,12 @@ public class StraysParser {
         int[] edgeDistance = new int[numWays];
 
         System.out.println("edges array allocated");
-        int edgesPos = 0;
-        int numberNodes = 0;
+
         HashMap<Long, Integer> nodeMap = new HashMap<>();
 
         // Reset iterator
         InputStream input = new FileInputStream(path);
         OsmIterator iterator = new PbfIterator(input, true);
-
 
         String highway;
         String sidewalk;
@@ -57,6 +56,9 @@ public class StraysParser {
         Map<String, String> WayTags;
         TLongList wayNodes;
         int wayNodesSize;
+        int currentEdgeIndex = 0;
+        int numberNodes = 0;
+
 
         // Create edges and store node IDs
         for (EntityContainer container : iterator) {
@@ -81,32 +83,27 @@ public class StraysParser {
                     wayNodes = OsmModelUtil.nodesAsList((OsmWay) container.getEntity());
                     wayNodesSize = wayNodes.size();
 
-                    if (!nodeMap.containsKey(wayNodes.get(0))) {
-                        nodeMap.put(wayNodes.get(0), numberNodes);
-                        numberNodes++;
-                        System.out.println(numberNodes);
-                    }
-                    for (int i = 1; i < wayNodesSize; i++) {
+                    for (int i = 0; i < wayNodesSize; i++) {
                         if (!nodeMap.containsKey(wayNodes.get(i))) {
                             nodeMap.put(wayNodes.get(i), numberNodes);
                             numberNodes++;
                         }
-                        edgeSource[edgesPos] = nodeMap.get(wayNodes.get(i - 1));
-                        edgeTarget[edgesPos] = nodeMap.get(wayNodes.get(i));
-                        edgesPos++;
+                        edgeSource[currentEdgeIndex] = nodeMap.get(wayNodes.get(i - 1));
+                        edgeTarget[currentEdgeIndex] = nodeMap.get(wayNodes.get(i));
+                        currentEdgeIndex++;
                     }
                     for (int i = wayNodesSize - 1; i > 0; i--) {
-                        edgeSource[edgesPos] = nodeMap.get(wayNodes.get(i));
-                        edgeTarget[edgesPos] = nodeMap.get(wayNodes.get(i - 1));
-                        edgesPos++;
+                        edgeSource[currentEdgeIndex] = nodeMap.get(wayNodes.get(i));
+                        edgeTarget[currentEdgeIndex] = nodeMap.get(wayNodes.get(i - 1));
+                        currentEdgeIndex++;
                     }
                 }
             }
         }
 
-        System.out.println(nodeMap.size());
+        System.out.println(String.format("added %d nodes", nodeMap.size()));
         double[][] nodes = new double[3][nodeMap.size()];
-        System.out.println("YAY");
+        System.out.println("Created nodes array");
 
         // Reset iterator
         input.close();
@@ -128,6 +125,8 @@ public class StraysParser {
             }
         }
 
+        System.out.println("Filled nodes array with data");
+
         // Calculate distance for each edge and then calculate the time needed to travel
         // along this edge.
         for (int i = 0; i < numWays; i++) {
@@ -141,12 +140,14 @@ public class StraysParser {
             edgeDistance[i] = (int) (dist * 10000);
         }
 
+        System.out.println("Filled edges array with distances");
 
         Integer[] ids = new Integer[numWays];
         for (int i = 0; i < numWays; i++)
             ids[i] = i;
         Arrays.sort(ids, Comparator.comparingInt(o -> edgeSource[o]));
 
+        System.out.println("sorted edges array");
 
         // Save data to file
         PrintWriter writer = new PrintWriter("ressources\\de_nodes_stray.fs",
@@ -170,6 +171,8 @@ public class StraysParser {
             writer.println(" ");
         }
         writer.close();
+
+        System.out.println("finished writing to file");
     }
 
     private static int countWays(Set<String> legalStreets) throws IOException {
@@ -194,61 +197,11 @@ public class StraysParser {
         return numWays;
     }
 
-    private static int getSpeedLimit(String maxspeedTag, String highway) {
-        if (maxspeedTag == null) {
-            return getSpeed(highway);
-        } else if (StringUtils.isNumeric(maxspeedTag)) {
-            return Integer.parseInt(maxspeedTag);
-        } else if (maxspeedTag.equals("none")) {
-            return 150;
-        } else if (maxspeedTag.equals("walk")) {
-            return 5;
-        } else {
-            return getSpeed(highway);
-        }
-    }
-
-    private static int getSpeed(String tag) {
-        switch (tag) {
-            case "motorway":
-                return 120;
-            case "motorway_link":
-                return 60;
-            case "trunk":
-                return 100;
-            case "trunk_link":
-                return 50;
-            case "primary":
-                return 60;
-            case "primary_link":
-                return 50;
-            case "secondary":
-                return 50;
-            case "secondary_link":
-                return 50;
-            case "tertiary":
-                return 50;
-            case "tertiary_link":
-                return 50;
-            case "unclassified":
-                return 40;
-            case "residential":
-                return 30;
-            case "service":
-                return 10;
-            case "living_street":
-                return 5;
-            default:
-                return 50;
-        }
-    }
-
     public static class ItemComparator implements Comparator<double[]> {
         @Override
         public int compare(double[] o1, double[] o2) {
             return Double.compare(o1[0], o2[0]);
         }
-
     }
 
     private static double euclideanDist(double node1_lat, double node1_lng, double node2_lat, double node2_lng) {
